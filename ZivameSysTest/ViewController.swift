ebfa55c4befb654633affff4a9a7e45524592771
varen1994 <varendersingh130@gmail.com>
2020-08-12 12:53:33 +0530

    Created and added Base Files of project

---
+++ ZivameSysTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZivameSysTest
+//
+//  Created by Varender Singh on 12/08/20.
+//  Copyright © 2020 Varender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
