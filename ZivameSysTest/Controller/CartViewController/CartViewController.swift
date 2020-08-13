61cf1176e5559c472ed8940a842a18a74a538a1f
varen1994 <varendersingh130@gmail.com>
2020-08-13 18:28:46 +0530

    1. Core data management Completed 2. Shown Count on Cells 3. Changed Product model structure

--- ZivameSysTest/Controller/CartViewController/CartViewController.swift
+++ ZivameSysTest/Controller/CartViewController/CartViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import SwiftMessages
 
-
 class CartViewController: UIViewController {
 
     var count:Int = 0
@@ -17,22 +16,56 @@ class CartViewController: UIViewController {
     @IBOutlet weak var cartTableView: UITableView!
     var cartData:CartData?
     var viewModel = CartViewModel()
+    lazy var activityIndicator:UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        activityIndicator.style = .large
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.center = self.view.center
+        activityIndicator.color = UIColor.blue
+        activityIndicator.stopAnimating()
+        return activityIndicator
+    }()
+    var refreshControl:UIRefreshControl?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.cartBarButton = CustomCartBarButton()
         self.navigationItem.rightBarButtonItem = self.cartBarButton
+        self.cartTableView.separatorStyle = .none
+        upDateCartItemsCount()
         registerForTheCell()
-        getCartDataFromViewModel()
+        getCartDataFromViewModel(fromPullRefresh:false)
+        self.view.insertSubview(self.activityIndicator, aboveSubview: self.cartTableView)
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl?.tintColor = UIColor.blue
+        self.cartTableView.refreshControl = self.refreshControl
+        self.refreshControl?.addTarget(self, action: #selector(self.pullRefreshSwiped), for: .valueChanged)
     }
     
-    func getCartDataFromViewModel() {
+    @objc func pullRefreshSwiped() {
+         getCartDataFromViewModel(fromPullRefresh:true)
+    }
+    
+    func getCartDataFromViewModel(fromPullRefresh:Bool) {
+        if fromPullRefresh == false {
+            self.activityIndicator.startAnimating()
+        }
         self.viewModel.getCartData { (data, error) in
             DispatchQueue.main.async {
+                if fromPullRefresh {
+                    self.refreshControl?.endRefreshing()
+                     self.activityIndicator.stopAnimating()
+                } else {
+                    self.activityIndicator.stopAnimating()
+                }
                 if let error = error {
                     SwiftMessages.show(view: Helper.createAnAlertForApiError(stringMessage:error))
                 } else {
                     self.cartData = data
+                    if (self.cartData?.products?.count ?? 0) != 0 {
+                        self.cartTableView.separatorStyle = .singleLine
+                        self.cartTableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0 )
+                    }
                     self.cartTableView.delegate = self
                     self.cartTableView.dataSource = self
                     self.cartTableView.reloadData()
@@ -44,7 +77,14 @@ class CartViewController: UIViewController {
     func registerForTheCell() {
         self.cartTableView.register(CartTableViewCell.nib(), forCellReuseIdentifier: CartTableViewCell.nibName())
     }
-
+    
+    func upDateCartItemsCount() {
+        if let items = DataManager.shared.getNumberOfWalletItems(),items > 0 {
+            self.cartBarButton?.changeTag(text: "\(items)")
+        } else {
+            self.cartBarButton?.changeTag(text: nil)
+        }
+    }
 }
 
 extension CartViewController:UITableViewDataSource,UITableViewDelegate {
@@ -69,49 +109,90 @@ extension CartViewController:CartTableViewCellDelegate {
     func moreButtonClicked(product: Product?) {
         guard let product = product else { return }
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        let addToWishListalertAction = UIAlertAction(title: "Add to Wishlist", style: .default) { _ in
+        
+        let addToCartalertAction = UIAlertAction(title: "Add to Cart", style: .default) { _ in
+            self.showAnimationOfProductLoadedToCart(product: product, decrement: false)
+        }
+        
+        let tupple = DataManager.shared.getProductsNumberOfItemInCartAndIfIsPresentInTheWishList(product: product)
+        let addToWishListalertAction = UIAlertAction(title: (tupple.1 == false ? "Add to wishlist" : "Remove from wishlist"), style: .default) { _ in
             self.showAnimationOfProductAddedToWishList(product: product)
         }
-        let addToCartalertAction = UIAlertAction(title: "Add to Cart", style: .default) { _ in
-            self.showAnimationOfProductLoadedToCart(product: product)
+        
+        let removeFromCart = UIAlertAction(title: "Remove from Cart", style: .default) { _ in
+            self.showAnimationOfProductLoadedToCart(product: product, decrement: true)
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
         alertController.addAction(addToWishListalertAction)
         alertController.addAction(addToCartalertAction)
+        
+        if tupple.0 > 0 {
+            alertController.addAction(removeFromCart)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alertController.addAction(cancelAction)
         self.present(alertController, animated: true, completion: nil)
     }
     
-    func showAnimationOfProductLoadedToCart(product:Product) {
-        if let products = self.cartData?.products {
-            if let index = products.firstIndex(where: {$0.name == product.name }) {
-                guard let cell = self.cartTableView.cellForRow(at: IndexPath(row: index, section: 0)) as? CartTableViewCell else { return }
-                let rect = self.cartTableView.rectForRow(at: IndexPath(row: index, section: 0))
-                var imageView:UIImageView? = UIImageView(frame: CGRect(x: cell.itemImageView.frame.origin.x, y: rect.origin.y + cell.itemImageView.frame.origin.y - self.cartTableView.contentOffset.y, width: cell.itemImageView.frame.width, height: cell.itemImageView.frame.height))
-                imageView?.image = cell.itemImageView.image
-                self.view.insertSubview(imageView!, aboveSubview: self.cartTableView)
-                UIView.animate(withDuration: 2.0, animations: {
-                    imageView!.frame = CGRect(x:UIScreen.main.bounds.width-30 , y: 30, width: 10, height: 10)
-                    imageView!.alpha = 0.25
-                }) { (_) in
-                    imageView!.removeFromSuperview()
-                    imageView = nil
-                    self.count += 1
-                    self.cartBarButton?.changeTag(text:"\(self.count)")
+    func showAnimationOfProductLoadedToCart(product:Product, decrement:Bool) {
+        if decrement == false {
+          product.numberOfItems = (product.numberOfItems ?? 0) + 1
+        } else {
+          product.numberOfItems = (product.numberOfItems ?? 0) - 1
+        }
+        if  DataManager.shared.incrementOrDecrementNumberOfItems(product: product) {
+            if let products = self.cartData?.products {
+                if let index = products.firstIndex(where: {$0.name == product.name }) {
+                    guard let cell = self.cartTableView.cellForRow(at: IndexPath(row: index, section: 0)) as? CartTableViewCell else { return }
+                    let rect = self.cartTableView.rectForRow(at: IndexPath(row: index, section: 0))
+                    
+                    if decrement == true {
+                        var imageView:UIImageView? = UIImageView(frame: CGRect(x:UIScreen.main.bounds.width-30 , y: 30, width: 10, height: 10))
+                        imageView?.image = cell.itemImageView.image
+                        self.showAnimationWithCompletionHandler(imageView: imageView, toFrame: CGRect(x: cell.itemImageView.frame.origin.x, y: rect.origin.y + cell.itemImageView.frame.origin.y - self.cartTableView.contentOffset.y, width: cell.itemImageView.frame.width, height: cell.itemImageView.frame.height)) { () -> (Void) in
+                           self.upDateCartItemsCount()
+                            cell.updateNumberOfItemsInCart()
+                             imageView = nil
+                        }
+                    } else {
+                        var imageView:UIImageView? = UIImageView(frame:CGRect(x: cell.itemImageView.frame.origin.x, y: rect.origin.y + cell.itemImageView.frame.origin.y - self.cartTableView.contentOffset.y, width: cell.itemImageView.frame.width, height: cell.itemImageView.frame.height))
+                        imageView?.image = cell.itemImageView.image
+                        self.showAnimationWithCompletionHandler(imageView: imageView, toFrame: CGRect(x:UIScreen.main.bounds.width-30 , y: 30, width: 10, height: 10) ) { () -> (Void) in
+                           self.upDateCartItemsCount()
+                           cell.updateNumberOfItemsInCart()
+                           imageView = nil
+                        }
+                    }
                 }
             }
+        } else {
+            
+        }
+    }
+    
+    func showAnimationWithCompletionHandler(imageView:UIImageView?,toFrame:CGRect,completionHandler:@escaping ()->(Void)) {
+        self.view.insertSubview(imageView!, aboveSubview: self.cartTableView)
+        self.view.isUserInteractionEnabled = false
+        UIView.animate(withDuration: 0.5, animations: {
+            imageView!.frame = toFrame
+            imageView!.alpha = 0.25
+        }) { (_) in
+            imageView!.removeFromSuperview()
+            self.view.isUserInteractionEnabled = true
+            completionHandler()
         }
     }
     
     func showAnimationOfProductAddedToWishList(product:Product) {
+        product.isWishListItem = !(product.isWishListItem ?? false)
+        if !DataManager.shared.addProductInToWishList(product: product) { return }
         if let products = self.cartData?.products {
             if let index = products.firstIndex(where: {$0.name == product.name }) {
                 guard let cell = self.cartTableView.cellForRow(at: IndexPath(row: index, section: 0)) as? CartTableViewCell else { return }
                 UIView.animate(withDuration: 2.0, animations: {
-                    cell.contentView.backgroundColor = UIColor.blue.withAlphaComponent(0.05)
-                }) { (_) in
-                    cell.contentView.backgroundColor = UIColor.white
-                }
+                    cell.updateWishList()
+                })
             }
         }
     }
--- ZivameSysTest/Controller/CartViewController/Cells/CartTableViewCell.swift
+++ ZivameSysTest/Controller/CartViewController/Cells/CartTableViewCell.swift
@@ -16,6 +16,7 @@ protocol CartTableViewCellDelegate:class {
 
 class CartTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var numberOfItemsInCartLabel: UILabel!
     @IBOutlet weak var outerView: UIView!
     @IBOutlet weak var itemTitleLabel: UILabel!
     @IBOutlet weak var itemImageView: UIImageView!
@@ -40,15 +41,35 @@ class CartTableViewCell: UITableViewCell {
         self.product = product
         self.itemTitleLabel.text = product.name ?? "N/A"
         self.itemPriceLabel.text = "₹" + (product.price ?? "0.0")
-        
         self.starView.rating = Double(product.rating ?? 0)
-        
+        self.updateNumberOfItemsInCart()
+        self.updateWishList()
         guard let url = URL(string: product.image_url!) else { return }
         self.itemImageView.sd_setImage(with: url, completed: nil)
     }
     
     @IBAction func moreButtonClicked(_ sender: Any) {
         self.delegate?.moreButtonClicked(product: self.product)
+    }
+    
+    func updateNumberOfItemsInCart() {
+        self.numberOfItemsInCartLabel.layer.cornerRadius = self.numberOfItemsInCartLabel.layer.frame.height/2
+        self.numberOfItemsInCartLabel.layer.masksToBounds = true
+        if let itemsCount = self.product?.numberOfItems,itemsCount > 0 {
+            numberOfItemsInCartLabel.isHidden = false
+           numberOfItemsInCartLabel.text = "\(itemsCount)"
+        } else {
+            numberOfItemsInCartLabel.text = nil
+            numberOfItemsInCartLabel.isHidden = true
+        }
+    }
+    
+    func updateWishList() {
+        if let value =  self.product?.isWishListItem,value == true {
+            self.contentView.backgroundColor = UIColor.blue.withAlphaComponent(0.05)
+        } else {
+            self.contentView.backgroundColor = UIColor.white
+        }
     }
 }
 
